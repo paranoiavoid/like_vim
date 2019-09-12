827a27d4f0e40f7ae5215ca51827c350c7ea8c16
paranoiavoid <k507593@kansai-u.ac.jp>
2019-09-12 22:23:18 +0900

    インサートモード用の関数の原型を用意

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <curses.h>
 
-
 enum MODE { //現在のモードの状態
     NOR,    //ノーマルモード
     INS,    //挿入モード
@@ -8,38 +7,65 @@ enum MODE { //現在のモードの状態
     COM,    //コマンドラインモード
 };
 
-char input(void) {
+int window_size_x; //画面の横幅
+int window_size_y; //画面の縦幅
+int cursor_x;      //カーソルのx座標
+int cursor_y;      //カーソルのy座標
+MODE mode;         //現在のモード
+
+int input_char(void) { //入力された(特殊)文字のキーコードを返す
     return getch();
 }
 
-int output(char c) {
-    int flag=0;
-    addch(c);
-    if(c=='\n'){
-        flag=1;
+void normal_mode(char c) {
+    if (c == 'i') {
+        mode = INS;
+    } else if (c == 'I') {
+        mode = INS;
+        cursor_x++;
     }
+}
+void insert_mode(int c) {
+    if (c == 27) { // Escキーが入力
+        mode = NOR;
+        cursor_x--;
+    } else {
+        addch((char)c);
+        cursor_x++;
+    }
+}
 
-    return flag;    
+void input_check(int c) {
+    if (mode == NOR) {
+        normal_mode(c);
+    } else if (mode == INS) {
+        insert_mode(c);
+    } else if (mode == VIS) {
+
+    } else if (mode == COM) {
+    }
 }
 
 int main(void) {
-    initscr();
+    initscr(); //初期化する
+
+    scrollok(stdscr, true); //ウィンドウのスクロールを有効にする
+    getmaxyx(stdscr, window_size_y,
+             window_size_x); //ウィンドウのサイズを取得する
 
     erase();
 
-    int x,y;
-    x=0;
-    y=0;
-    while(1) {
-        move(y,x);
-        if(output(input())==1) {
-            break;
-        }
-        y++;
+    cursor_x = 0;
+    cursor_y = 0;
+    noecho();             //入力された文字を画面に表示しない
+    keypad(stdscr, true); //特殊なキーコードを使うようにする
+    while (1) {
+        move(cursor_y, cursor_x);
+        input_check(input_char());
     }
 
     timeout(-1);
-    y++;
+    cursor_y++;
     addstr("END");
     getch();
 
